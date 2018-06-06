5727e3f2525784950cd70937d8ce3fe75dacb1df
ryota.nagatomo <ec2-user@ip-172-31-46-39.us-east-2.compute.internal>
2018-06-06 06:56:27 +0000

    post

--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -10,6 +10,7 @@
                 </button>
                 <a class="navbar-brand" href="/">Microposts</a>
             </div>
+            
             <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul class="nav navbar-nav navbar-right">
                     
